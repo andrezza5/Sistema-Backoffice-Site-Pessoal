9929c6a263072cc6a0a58b5e7b8bb7173fa6c52c
andrezza5 <andrezzamarcilio@gmail.com>
2023-05-15 14:24:10 -0300

    atual

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://server-rgq0.onrender.com",
+    baseURL: "https://server-a-xhpr.onrender.com",
 });
 
 export default api;
